aca9ccb0f0081ffe621cb3c32fc2498078813493
任崇庆 <chrisren@lagou.com>
2016-04-20 11:52:42 +0800

    first commit

---
+++ CCHelloWorld/CCHelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCHelloWorld
+//
+//  Created by Chris on 16/4/20.
+//  Copyright © 2016年 CC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CCHelloWorld/CCHelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCHelloWorld
+//
+//  Created by Chris on 16/4/20.
+//  Copyright © 2016年 CC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
